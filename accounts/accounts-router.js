3c9c354a2cf552ebb05be8445f422f7c9f287b62
Eian Carter <eian@bricleir.com>
2020-02-17 15:10:06 -0800

    added all routes for account

---
+++ accounts/accounts-router.js
@@ -0,0 +1,78 @@
+const express = require("express");
+
+const db = require("../data/dbConfig");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    db.select("*")
+        .from("accounts")
+        .then(accounts => {
+            res.status(200).json(accounts);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ error: "failed to get accounts" })
+        });
+});
+
+router.get("/:id", (req, res) => {
+    getById(req.params.id)
+        .then(account => {
+            res.status(200).json(account);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ message: "failed to get account" })
+        });
+});
+
+router.post("/", (req, res) => {
+    db.insert(req.body, "id")
+        .then(ids => {
+            return getById(ids[0]).then(inserted => {
+                res.status(201).json(inserted);
+            });
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ error: "failed to add the account" })
+        });
+});
+
+router.put("/:id", (req, res) => {
+    const id = req.params.id;
+    const changes = req.body;
+    db("posts")
+        .where({ id })
+        .update(changes)
+        .then(count => {
+            res.status(200).json(count);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ error: "failed to update the account" })
+        });
+});
+
+router.delete("/:id", (req, res) => {
+    const id = req.params.id;
+    db("posts")
+        .where({ id })
+        .del()
+        .then(count => {
+            res.status(200).json(count);
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ error: "failed to delete the account" })
+        });
+});
+
+module.exports = router;
+
+function getById(id) {
+    return db("posts")
+        .where({ id })
+        .first();
+}
--- queries.sql
+++ queries.sql
@@ -3,25 +3,25 @@
 -- Find all customers with postal code 1010
 SELECT * 
 FROM Customers 
-WHERE PostalCode='1010';
+WHERE PostalCode = '1010';
 
 -- Find the phone number for the supplier with the id 11
-SELECT Phone 
-FROM Suppliers 
-WHERE SupplierID='11';
+SELECT Phone
+FROM Suppliers
+WHERE SupplierID = '11';
 
 -- List first 10 orders placed, sorted descending by the order date
 SELECT * 
-FROM Orders
-ORDER BY OrderDate desc,
+FROM Orders 
+ORDER BY OrderDate DESC 
 LIMIT 10
 
 -- Find all customers that live in London, Madrid, or Brazil
-SELECT *
+SELECT * 
 FROM Customers
 WHERE City = 'London'
 OR City = 'Madrid'
-OR Country = 'Brazil'
+OR Country = 'Brazil';
 
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 INSERT INTO Customers (Country, CustomerName, ContactName, Address, City, PostalCode)
